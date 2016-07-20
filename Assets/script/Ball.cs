3d0afa7739f83e06e91f1e764cf1e68c10b663d2
ato <ato.dark.6603.s@gmail.com>
2016-07-21 08:39:00 +0900

    とりあえずあげ

---
+++ Assets/script/Ball.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class Ball : MonoBehaviour
+{
+
+    public float ballInitialVelocity = 600f;
+
+
+    private Rigidbody rb;
+    private bool ballInPlay;
+
+    void Awake()
+    {
+
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && ballInPlay == false)
+        {
+            transform.parent = null;
+            ballInPlay = true;
+            rb.isKinematic = false;
+            rb.AddForce(new Vector3(ballInitialVelocity*2, 0, ballInitialVelocity*2));
+        }
+    }
+}
---
+++ Assets/script/Paddle.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour
+{
+
+    public float paddleSpeed = 1f;
+
+
+    private Vector3 playerPos = new Vector3(0, 1f, -9.5f);
+
+    void Update()
+    {
+        float xPos = transform.position.x + (Input.GetAxis("Horizontal") * paddleSpeed);
+        playerPos = new Vector3(Mathf.Clamp(xPos, -15f, 15f), 1f, -9.5f);
+        transform.position = playerPos;
+
+    }
+}
