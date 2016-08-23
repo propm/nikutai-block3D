55faff9d36e3d9d610ae6664b5bc2a95923d85e0
fmacro <fmacro00@gmail.com>
2016-08-23 13:15:59 +0900

    ブロックの破壊によるスコアの加算処理を追加

--- Assets/script/Block.cs
+++ Assets/script/Block.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class Block : MonoBehaviour {
-
+	public int AddScore = 500;
 	// Use this for initialization
 	void Start () {
 	
@@ -16,6 +16,7 @@ public class Block : MonoBehaviour {
 		//衝突判定
 		if (collision.gameObject.tag == "Ball") {
 			//相手のタグがBallならば、自分を消す
+			MainGameData.SetScore(MainGameData.GetScore()+AddScore); 
 			Destroy(this.gameObject);
 		}
 	}
---
+++ Assets/script/ScoreView.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreView : MonoBehaviour {
+	private int score;
+	public Text ScoreText;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		score = MainGameData.GetScore ();
+		ScoreText.text = "Score: " + score.ToString();
+	}
+}
