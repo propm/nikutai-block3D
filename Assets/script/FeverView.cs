de42f4894666eabd6ee3444345d6f6428de3460f
fmacro <fmacro00@gmail.com>
2016-08-26 16:51:25 +0900

    簡易的なフィーバー表示を追加

---
+++ Assets/script/FeverView.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FeverView : MonoBehaviour {
+	private int Fever;
+	private bool isFever;
+	public Text FeverText;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Fever = MainGameData.GetFever ();
+		isFever = MainGameData.GetisFever ();
+		if (isFever == true) {
+			FeverText.text = "Fever!";
+		} else {
+			FeverText.text = "Fever: " + Fever.ToString ();
+		}
+	}
+}
