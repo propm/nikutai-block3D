9366a6a89aa2360a09d4bcc0eaa061a94ea1077d
fmacro <fmacro00@gmail.com>
2016-08-26 16:38:46 +0900

    フィーバー状態でのブロックの貫通処理を追加

--- Assets/script/Block.cs
+++ Assets/script/Block.cs
@@ -3,15 +3,24 @@ using System.Collections;
 
 public class Block : MonoBehaviour {
 	public int AddScore = 500;
+	public int FeverAddScore = 750;
+	public GameObject thisBlock;
+	Collider BlockCollider;
 	// Use this for initialization
 	void Start () {
-	
+		BlockCollider = thisBlock. GetComponent<Collider>();
 	}
-	
+
+
 	// Update is called once per frame
 	void Update () {
-	
+		if (MainGameData.GetisFever () == true) {
+			BlockCollider.isTrigger = true;
+		} else {
+			BlockCollider.isTrigger = false;
+		}
 	}
+
 	void OnCollisionEnter(Collision collision) {
 		//衝突判定
 		if (collision.gameObject.tag == "Ball") {
@@ -20,6 +29,13 @@ public class Block : MonoBehaviour {
 			if (MainGameData.GetisFever () == false) {
 				MainGameData.SetFever (MainGameData.GetFever () + 4);
 			}
+			Destroy(this.gameObject);
+		}
+	}
+
+	void OnTriggerEnter(Collider trigger){
+		if(trigger.gameObject.tag== "Ball"){
+			MainGameData.SetScore(MainGameData.GetScore()+FeverAddScore); 
 			Destroy(this.gameObject);
 		}
 	}
