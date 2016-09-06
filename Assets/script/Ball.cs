5c598dd95b01b55c1473e42f113edfa78baaebf9
fmacro <fmacro00@gmail.com>
2016-09-06 10:51:18 +0900

    視点を調整

--- Assets/script/Ball.cs
+++ Assets/script/Ball.cs
@@ -11,7 +11,7 @@ public class Ball : MonoBehaviour
 
     private Rigidbody rb;
     private bool ballInPlay;
-	private GameObject ball;
+	GameObject ball;
 
     void Start()
     {
@@ -22,8 +22,7 @@ public class Ball : MonoBehaviour
 
     void Update()
     {
-		ballInPlay = MainGameData.GetBallInPlay();
-        if (Input.GetButtonDown("Fire1") && ballInPlay == false)
+		if (Input.GetButtonDown("Fire1") && MainGameData.GetBallInPlay() == false)
         {
             transform.parent = null;
 			MainGameData.SetBallInPlay (true);
@@ -33,17 +32,24 @@ public class Ball : MonoBehaviour
 
     }
 
-	void onCollisionEnter(){
-		Vector3 ballvel = transform.gameObject.GetComponent<Rigidbody>().velocity;
-		ballvel = ballvel.normalized * 15;
+	void onCollisionEnter(Collision collision){
+		if (collision.gameObject.tag == "Out") {
+			ball.transform.position = new Vector3 (0f, 1f, 0f);
+			ball.transform.gameObject.GetComponent<Rigidbody> ().velocity = new Vector3 (0f, 0f, 0f);
+			MainGameData.SetBallInPlay (false);
+		} else {
+			Vector3 ballvel = transform.gameObject.GetComponent<Rigidbody> ().velocity;
+			ballvel = ballvel.normalized * 15;
 
-		if (Mathf.Abs (ballvel.y) < 5) {
-			ballvel.z *= 5;
+			if (Mathf.Abs (ballvel.y) < 5) {
+				ballvel.z *= 5;
+			}
+			if (Mathf.Abs (ballvel.x) < 5) {
+				ballvel.x *= 5;
+			}
+			transform.gameObject.GetComponent<Rigidbody> ().velocity = ballvel;
 		}
-		if (Mathf.Abs (ballvel.x) < 5) {
-			ballvel.x *= 5;
-		}
-		transform.gameObject.GetComponent<Rigidbody>().velocity = ballvel;
+
 	}
 
 }
