b3b75fa554e02f767b0b57e936e49532f2c7856c
fmacro <fmacro00@gmail.com>
2016-09-06 11:24:35 +0900

    ボールが極端に水平、垂直方向に傾く挙動を修正（要テスト）

--- Assets/script/Ball.cs
+++ Assets/script/Ball.cs
@@ -33,23 +33,31 @@ public class Ball : MonoBehaviour
     }
 
 	void onCollisionEnter(Collision collision){
-		if (collision.gameObject.tag == "Out") {
+		if (collision.gameObject.tag == "Respawn") {
 			ball.transform.position = new Vector3 (0f, 1f, 0f);
 			ball.transform.gameObject.GetComponent<Rigidbody> ().velocity = new Vector3 (0f, 0f, 0f);
 			MainGameData.SetBallInPlay (false);
-		} else {
-			Vector3 ballvel = transform.gameObject.GetComponent<Rigidbody> ().velocity;
-			ballvel = ballvel.normalized * 15;
+		} 
+		velocityCtrl ();
 
-			if (Mathf.Abs (ballvel.y) < 5) {
-				ballvel.z *= 5;
-			}
-			if (Mathf.Abs (ballvel.x) < 5) {
-				ballvel.x *= 5;
-			}
-			transform.gameObject.GetComponent<Rigidbody> ().velocity = ballvel;
+	}
+
+	private void velocityCtrl(){
+		Vector3 v = rb.velocity;	// 速度を取得.
+
+		if(-3.0f < v.x && v.x <= 0.0f){			// Xの速度が-3～0なら.
+			v.x = -3.0f;							// Xの値を -3.0f に.
+		}else if(0.0f < v.x && v.x < 3.0f ){	// Xの速度が0～3なら).
+			v.x =  3.0f;							// Xの値を +3.0f に.
 		}
 
+		if(-10.0f < v.y &&  v.y <= 0.0f){		// Yの速度が-10～0なら.
+			v.y = -10.0f;							// Yの値を -10.0f に.
+		}else if(0.0f < v.y && v.y < 10.0f){	// Yの速度が0～10なら).
+			v.y = 10.0f;							// Yの値を +10.0f に.
+		}
+
+		rb.velocity = v;	// 値を反映.
 	}
 
 }
