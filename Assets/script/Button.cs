2f2565adbb3439a31bfc903e2007ed583df93113
稲木 奎冴 <keigo12290@gmail.com>
2016-10-15 15:42:29 +0900

    スコア画面にリトライボタンを追加

---
+++ Assets/script/Button.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour {
+
+    // ボタンをクリックした時の処理
+    public void ButtonPush()
+    {
+        Debug.Log("clicked");
+        Application.LoadLevel("scene1");
+        Application.LoadLevel("ScoreScene");
+        SceneManager.LoadScene("scene1");
+    }
+}
