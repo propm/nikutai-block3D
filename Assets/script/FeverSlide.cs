92f3a5b5a36ffd64a5440ae38460d12e1939ae85
稲木 奎冴 <keigo12290@gmail.com>
2016-10-29 17:07:46 +0900

    フィーバースライダーの仕様変更

--- Assets/script/FeverSlide.cs
+++ Assets/script/FeverSlide.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 
 public class FeverSlide : MonoBehaviour {
     public Slider slider;
+    float feverCage = 50f;
 	// Use this for initialization
 	void Start () {
         
@@ -11,6 +12,13 @@ public class FeverSlide : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        if (MainGameData.GetisFever()) {
+            feverCage -= 0.1f;
+            slider.value = feverCage;
+        } else {
             slider.value = MainGameData.GetFever();
+            feverCage = 50f;
+        }
+        
     }
 }
